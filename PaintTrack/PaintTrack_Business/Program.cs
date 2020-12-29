7f12fbe6a443b2a01f06ebe448fcad6d26e22b53
Dragonkid1996 <djnewell@btinternet.com>
2020-12-29 15:03:14 +0000

    Initial Commit

---
+++ PaintTrack/PaintTrack_Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaintTrack_Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
